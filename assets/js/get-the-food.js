fcbbd4a43694cbbd45d0b6b2d74d42eb8ff519a8
Kenneth Adjei <[kmadjei25@gmail.com]>
2021-05-08 00:14:59 -0400

    completed styling the food modal

--- assets/js/get-the-food.js
+++ assets/js/get-the-food.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
     //Open food modal when user clicks the food button
     $('#food').click(function(){
         $('#food-modal').css('display', 'block'); 
+        get_meal();
     }); 
 
     //closes the food modal when user clicks the close <span>
@@ -22,25 +23,88 @@ $(document).ready(function(){
         }
     }
 
+    // grabs more meal request when user clicks button
+    $('#food-modal #get-meal').click(get_meal);
+
     //executes get meal HTTP request
     function get_meal() {
 
-        let request;
 
+        // http API get request
         fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-        .then(response => { 
-            /* code if successful */ 
+        .then(response => response.json())
+        .then(data => {
+            //code if success
+            console.debug(data.meals[0]);
+            // function to html content
+            create_meal_html_content(data.meals[0])
+        }).catch(error => {
+            //code if request fails
+            console.error('Request failure: ', error);
+            //Print error message in html document
+            $('#food-modal .modal-body').html(`<p>${error.message}</p>`);
+        });
 
-            // convert to json object
-            request = response.json();
-            console.log(request[0][]);
 
-        },
-            error => {
-                /* code if some error */
-                console.error('Request failure: ', error);
-            }
-        );
     }
+
+
+
+    function create_meal_html_content(meal){
+
+        // idea for creating this function was based on the meal API link below
+        // https://javascript.plainenglish.io/5-fun-apis-for-your-next-javascript-projects-1834626864c
+        
+        let ingredients = [];
+        
+        //loop to build ingredients list
+        for(let i = 0; i < 20; i++ ){
+            
+            if (meal[`strIngredient${i + 1}`]) {
+                //if an ingredient is found in the JSON meal API object
+                //append it
+                ingredients[i] = `<li>${meal[`strIngredient${i + 1}`]} - ${meal[`strMeasure${i + 1}`]}</li>`;
+            } else {
+                //break out of the loop if no ingredient
+                break;
+            }       
+        }
+
+        // HTML template for the meal content
+        let mealHTML = `
+            <section class="caption">
+                <div class="thumbnail">
+                    <img src="${meal.strMealThumb}" alt="Food Image">
+                </div>
+                <div class="info">
+                    <p class="food-id">
+                        ${meal.strArea ? `<b>Origin:</b> ${meal.strArea}` : ""}
+                        ${meal.strCategory ? `<b>Category:</b> ${meal.strCategory}` : ""}
+                        <b>Meal:</b> ${meal.strMeal}
+                    </p>
+                    <div class="ingredients">
+                        <h4>Ingredients</h4>
+                        <ul>${ingredients.join("")}</ul>
+                    </div>
+                </div>
+            </section>
+            <h3>Instructions</h3>
+            <p class="instructions">${meal.strInstructions}</p>
+            ${meal.strYoutube ? 
+                `<h3>Video Preparation</h3>
+                <div class="video-wrapper">
+                    <iframe src="https://www.youtube.com/embed/${meal.strYoutube.slice(32)}" title="Cooking preparation video">
+                        <p>Sorry, Your browser does not support iframe element!</p>
+                    </iframe>
+                </div>` : 
+                ""}
+        `;
+
+        // displays the result in the HTML document
+        $('#food-modal .modal-body').html(mealHTML);
+
+    }
+
+    
     
 });
